8581fc5cb22002ba313eb4fe535dffe59bc5abd0
Joao Pedro <jplm1970@gmail.com>
2018-08-24 15:39:07 +0000

    end of day 2 part 2

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       resources :users, only: [:show, :create, :update, :destroy]
 =======
       resources :users, only:[:show, :create, :update, :destroy]
->>>>>>> adding-users
+
     end
 
 
