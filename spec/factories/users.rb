d44d60410b00fa686bfacc2fd7b54a77648e9aee
Joao Pedro <jplm1970@gmail.com>
2018-08-24 10:32:32 +0000

    Change double quotes to simple quotes

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     email {Faker::Internet.email}
-    password "estoril"
-    password_confirmation "estoril"
+    password 'estoril'
+    password_confirmation 'estoril'
   end
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -5,29 +5,29 @@ RSpec.describe 'Users Api', type: :request do
   let!(:user) { FactoryGirl.create(:user)}
   let(:user_id) { user.id}
 
-  before { host! "api.task-manager.local"}
+  before { host! 'api.task-manager.local'}
 
-  describe "GET /users/:id" do
+  describe 'GET /users/:id' do
     before do
-      headers = { "Accept" => "application/vnd.taskmanager.v1"}
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1'}
       get "/users/#{user_id}", params: {}, headers: headers
     end
 
-    context "when the user exists" do
-      it "returns the user" do
+    context 'when the user exists' do
+      it 'returns the user' do
       user_response =  JSON.parse(response.body)
-      expect(user_response["id"]).to eq(user_id)
+      expect(user_response['id']).to eq(user_id)
       end
 
-      it "returns status code 200" do
+      it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
     end
 
-    context "when the user not exists" do
+    context 'when the user not exists' do
       let(:user_id) { 10000 }
 
-      it "returns satatus code 404" do
+      it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
     end
